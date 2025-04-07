ad4ab3e306d9b66a79f40a42b33bc6e7a23b0bf0
Shreyy <shreyw134@gmail.com>
2025-04-07 14:02:28 +0530

    Add files via upload

---
+++ test-codes/t3after.cpp
@@ -0,0 +1,6 @@
+extern "C" int superadd5(int x);
+
+int main() {
+    int x = 5;
+    return superadd5(x);
+}
---
+++ test-codes/t3after.s
@@ -0,0 +1,20 @@
+	.text
+	.def	@feat.00;
+	.scl	3;
+	.type	0;
+	.endef
+	.globl	@feat.00
+.set @feat.00, 0
+	.file	"t3after.cpp"
+	.def	main;
+	.scl	2;
+	.type	32;
+	.endef
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+main:                                   # @main
+# %bb.0:
+	movl	$5, %ecx
+	jmp	superadd5                       # TAILCALL
+                                        # -- End function
+	.addrsig
